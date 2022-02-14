1706f94c70d0ab7289423af2db8605e7a304307c
clinton wambugu <clintonwambugu@gmail.com>
2022-02-14 20:17:07 +0300

    Fix minor bug in display

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const form = document.querySelector(".top-banner form");
-
+const input = document.querySelector(".top-banner input");
+const apiKey = "981282f28b48aedaf9b5d1c32c638d52"; 
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
 
 form.addEventListener("submit", e => {
   e.preventDefault();
-  const inputVal = form[0].value;
-  const apiKey = "981282f28b48aedaf9b5d1c32c638d52";
+  const inputVal = input.value;
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
-
+  const listItems = list.querySelectorAll(".jax-section .city");
+  
   fetch(url)
     .then (res  => res.json())
     .then( data => {
-      // do something
       const {main, name, sys, weather} = data;
-      const icon = `https://openweathermap.org/img/wn/${
-        weather[0]["icon"]}@2x.png`;
+      const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
       const li = document.createElement("li");
       li.classList.add("city");
       
       const markup  = `
-        <h2 class="city-name" data-name="${name}, ${sys.country}">
+        <h2 class="city-name" data-name="${name},${sys.country}">
           <span>${name}</span>
           <sup>${sys.country}</sup>
         </h2>
@@ -27,15 +28,18 @@ form.addEventListener("submit", e => {
         </div>
         <figure>
           <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-          <figcaption>${weather[0][description]}</figcaption>
+          <figcaption>${weather[0]["description"]}</figcaption>
         </figure>
       `;
       li.innerHTML = markup;
-      list.appendChild(li); // where the fuck is list coming from
+      list.appendChild(li);
     })
     .catch(() => {
-      message.textContent = "Please search for a valid city"; // same with msg
+      msg.textContent = "Please search for a valid city";
     })
 
+    msg.textContent = "";
+    form.reset();
+    input.focus();
 });
 
